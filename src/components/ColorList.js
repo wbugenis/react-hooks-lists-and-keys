e2da2f2c6688e4c94a72456e3e7c7969503bfa09
William Bugenis <wbugenis@gmail.com>
2021-03-17 10:13:51 -0400

    commit tree dff6a6036886d9af60e82abb7af4f370c3bcba1d

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,14 +9,12 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorEls = colors.map(color => <li key = {color} style={{color:color}}>{color}</li>)
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <li style={{ color: colors[0] }}>{colors[0]}</li>
-      <li style={{ color: colors[1] }}>{colors[1]}</li>
-      <li style={{ color: colors[2] }}>{colors[2]}</li>
-      <li style={{ color: colors[3] }}>{colors[3]}</li>
-      <li style={{ color: colors[4] }}>{colors[4]}</li>
+      {colorEls}
     </div>
   );
 }
